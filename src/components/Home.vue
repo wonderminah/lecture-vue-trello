86ae9f476b2a99ac0d7c8fd4302d87cb8381e46b
Minah Kim <kimma1205@gmail.com>
2023-01-18 00:09:15 +0900

    Axios 강의 실습

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,8 @@
         <div>
             Board List:
             <div v-if="loading">loading...</div>
-            <div v-else>
-                API result: {{ apiRes }}
-            </div>
+            <div v-else>API result: {{ apiRes }}</div>
+            <div v-if="error">{{ error }}</div>
             <ul>
                 <li>
                     <router-link to="board/1">Board 1</router-link>
@@ -20,6 +19,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
@@ -33,18 +34,16 @@ export default {
     methods: {
         fetchData() {
             this.loading = true
-            const req = new XMLHttpRequest()
-            req.open('GET', 'http://localhost:3000/health')
-            // 완료되면 load라는 이벤트가 발생한다.
-            req.send() 
-            // 그 load 이벤트를 여기서 잡는다.
-            req.addEventListener('load', () => { 
+
+            axios.get('http://localhost:3000/health')
+            .then(res => {
+                this.apiRes = res.data
+            })
+            .catch(res => {
+                this.error = res.response.data
+            })
+            .finally(() => {
                 this.loading = false
-                this.apiRes = {
-                    status: req.status,
-                    statusText: req.statusText,
-                    response: JSON.parse(req.response)
-                }
             })
         }
     }
