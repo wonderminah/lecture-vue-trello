e226e820d17ae7881e5560989588f97149ddea02
Minah Kim <kimma1205@gmail.com>
2023-01-17 23:52:26 +0900

    백엔드 API 살펴보기, Ajax - HttpXMLRequest 객체 강의 실습

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,11 @@
     <div>
         Home
         <div>
+            Board List:
+            <div v-if="loading">loading...</div>
+            <div v-else>
+                API result: {{ apiRes }}
+            </div>
             <ul>
                 <li>
                     <router-link to="board/1">Board 1</router-link>
@@ -16,7 +21,33 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            loading: false,
+            apiRes: ''
+        }
+    },
+    created() {
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            this.loading = true
+            const req = new XMLHttpRequest()
+            req.open('GET', 'http://localhost:3000/health')
+            // 완료되면 load라는 이벤트가 발생한다.
+            req.send() 
+            // 그 load 이벤트를 여기서 잡는다.
+            req.addEventListener('load', () => { 
+                this.loading = false
+                this.apiRes = {
+                    status: req.status,
+                    statusText: req.statusText,
+                    response: JSON.parse(req.response)
+                }
+            })
+        }
+    }
 }
 </script>
 
